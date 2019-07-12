61185f8cdd2ff59d77a2c40a6dfac1dfa1314251
Luc Everse <luc@wukl.net>
2019-07-12 14:04:32 +0200

    Implement factory auto-instantiation

--- src/main/java/net/wukl/cacodi/IDependencyResolver.java
+++ src/main/java/net/wukl/cacodi/IDependencyResolver.java
@@ -61,6 +61,19 @@ public interface IDependencyResolver {
     <S> void addFactory(Class<S> iface, Factory<S> factory);
 
     /**
+     * Registers a factory for the service.
+     *
+     * The factory class is instantiated by the resolver before the factory is registered.
+     *
+     * @param iface the interface the service implements
+     * @param factoryClass the factory class to instantiate and register
+     * @param <S> the type of the service interface
+     */
+    default <S> void addFactory(final Class<S> iface, final Class<Factory<S>> factoryClass) {
+        this.addFactory(iface, this.get(factoryClass));
+    }
+
+    /**
      * Registers a factory for a service, but only if there was no other factory in place.
      *
      * The factory must return a type implementing the interface, otherwise a compile-time error is
@@ -73,6 +86,20 @@ public interface IDependencyResolver {
      * @return the factory currently in the resolver, after updating
      */
     <S> Factory<S> addDefaultFactory(Class<S> iface, Factory<S> factory);
+
+    /**
+     * Registers a factory for the service, but only if there was no other factory in place.
+     *
+     * The factory class is instantiated by the resolver before the factory is registered and
+     * only if no other factory was in place.
+     *
+     * @param iface the interface the service implements
+     * @param factoryClass the factory class to instantiate and register
+     * @param <S> the type of the service interface
+     *
+     * @return the factory currently in the resolver, after updating
+     */
+    <S> Factory<S> addDefaultFactory(Class<S> iface, Class<Factory<S>> factoryClass);
 
     /**
      * Registers an interface implementation.
